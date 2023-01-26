c4c78b89322b1746ab5da89535967ac5bc6c0aa0
Brendan Nolan <bnolan23@gmail.com>
2023-01-26 19:16:28 +0000

    Develop Further

--- src/queen_placer.rs
+++ src/queen_placer.rs
@@ -1,7 +1,9 @@
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Hash, Eq, Copy, Clone)]
 pub struct Row(pub usize);
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 pub struct Column(pub usize);
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
@@ -20,31 +22,47 @@ enum SquareStatus {
 struct QueenPlacer {
     queen_positions: Vec<Square>,
     size: usize,
+    columns_available: HashSet<Column>,
 }
 
 impl QueenPlacer {
+    fn place_queens(mut self) -> Vec<Square> {
+        Vec::new()
+    }
+
     fn new(size: usize) -> Self {
         QueenPlacer {
             queen_positions: Vec::new(),
             size,
+            columns_available: (0..size).map(Column).collect(),
         }
     }
 
-    fn place_queens(&mut self) -> Vec<Square> {
-        Vec::new()
+    fn next_row_for_placement(&self) -> Option<Row> {
+        let rows_filled = self.queen_positions.len();
+        if rows_filled == self.size {
+            None
+        } else {
+            Some(Row(self.queen_positions.len()))
+        }
     }
 
-    fn queens_left_to_add(&self) -> bool {
-        self.queen_positions.len() < self.size
-    }
-
-    fn place_queen(&self, square: Square) {
-        assert!(self.queens_left_to_add());
+    fn attempt_place_queen(&mut self, square: &Square) -> bool {
         assert!(square.row == Row(self.queen_positions.len()));
+        self.columns_available.remove(&square.column);
+        true
     }
 }
 
+fn queens_attack(queen_a: &Square, queen_b: &Square) -> bool {
+    if queen_a.row == queen_b.row || queen_a.column == queen_b.column {
+        return true;
+    }
+    (queen_a.row.0 as i32 - queen_b.row.0 as i32).abs()
+        == (queen_a.column.0 as i32 - queen_b.column.0 as i32).abs()
+}
+
 pub fn place_queens(size: usize) -> Vec<Square> {
-    let mut placer = QueenPlacer::new(size);
+    let placer = QueenPlacer::new(size);
     placer.place_queens()
 }
